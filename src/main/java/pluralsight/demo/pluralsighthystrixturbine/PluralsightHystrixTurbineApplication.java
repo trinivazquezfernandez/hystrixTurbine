4cf8ecb434d3633588f7e1e5428b64524910db18
trini <trinivazquezfernandez84@gmail.com>
2020-10-28 12:20:35 +0100

    hystrix turbine with rabbit tryed but not geted

---
+++ src/main/java/pluralsight/demo/pluralsighthystrixturbine/PluralsightHystrixTurbineApplication.java
@@ -0,0 +1,15 @@
+package pluralsight.demo.pluralsighthystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@EnableTurbineStream
+@SpringBootApplication
+public class PluralsightHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightHystrixTurbineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pluralsight/demo/pluralsighthystrixturbine/PluralsightHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package pluralsight.demo.pluralsighthystrixturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightHystrixTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
